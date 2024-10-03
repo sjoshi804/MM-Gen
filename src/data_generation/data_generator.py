19cb1ff104a22b02c925b9dd56ace138e5d9c991
Siddharth Joshi <sjoshi804@gmail.com>
2024-10-02 21:34:16 -0700

    bug fix

--- src/data_generation/data_generator.py
+++ src/data_generation/data_generator.py
@@ -349,7 +349,7 @@ class MultimodalDataGenerator:
         
         # DT string to associate examples with time of run
         self.dt_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-        self.gen_data_path = os.path.join(args.output_dir, SAVE_PATH, f"{args.file_prefix}_{self.dt_str}.json")
+        self.gen_data_path = os.path.join(args.output_folder, SAVE_PATH, f"{args.file_prefix}_{self.dt_str}.json")
         os.makedirs(self.gen_data_path, exist_ok=True)
 
     def generate_questions(self):
